a3560a478bb3618b7615007bf1fd77bc65a5f694
Mrbabet <wieliczkokamil@gmail.com>
2023-03-20 19:28:07 +0100

    Added NotFound Page backup to the home page

--- src/pages/notFound/NotFound.jsx
+++ src/pages/notFound/NotFound.jsx
@@ -1,6 +1,14 @@
 import "./notFound.scss";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/");
+    }, 3000);
+  });
   return <div>NotFound</div>;
 };
 
