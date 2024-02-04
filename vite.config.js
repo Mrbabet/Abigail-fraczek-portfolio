f5c6506e5b40cda5840323879e6c3f829f2a3aa1
Mrbabet <wieliczkokamil@gmail.com>
2024-02-04 15:55:01 +0100

    .

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ export default defineConfig({
   plugins: [react()],
   base: "/Abigail-fraczek-portfolio/",
   server: {
-    // Handle fallback for history API routing
     historyApiFallback: true,
   },
 });
